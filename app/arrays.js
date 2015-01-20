0ab1adcfcc48193172c6f1b8b85c7fd0113f9a9f
concreted <arichuang@gmail.com>
2015-01-19 17:02:47 -0800

    Implement indexOf

--- app/arrays.js
+++ app/arrays.js
@@ -3,11 +3,16 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     indexOf : function(arr, item) {
-
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === item) {
+          return i;
+        }
+      }
+      return -1;
     },
 
     sum : function(arr) {
-
+      
     },
 
     remove : function(arr, item) {
