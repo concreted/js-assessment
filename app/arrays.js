7d9d92a529442ab92a12dcdc942b56349cffc992
concreted <arichuang@gmail.com>
2015-01-19 17:36:05 -0800

    Implement curtail, concat, insert, count

--- app/arrays.js
+++ app/arrays.js
@@ -52,19 +52,23 @@ define(function() {
     },
 
     curtail : function(arr) {
-
+      arr.shift();
+      return arr;
     },
 
     concat : function(arr1, arr2) {
-
+      return arr1.concat(arr2);
     },
 
     insert : function(arr, item, index) {
-
+      arr.splice(index, 0, item);
+      return arr;
     },
 
     count : function(arr, item) {
-
+      return arr.reduce(function(counter, current) {
+        return current === item ? counter + 1 : counter;
+      }, 0);
     },
 
     duplicates : function(arr) {
